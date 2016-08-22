8da6573eceae0e8b3744961573dd1d2ac7e31066
Dan Breczinski <pt2323@gmail.com>
2016-08-22 12:39:30 -0400

    add js minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,28 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+
+var MAIN_JS_FILE = './js/main.js';
 
 gulp.task('scripts', function(){
     var b = browserify();
-    b.add('./js/main.js');
+    b.add(MAIN_JS_FILE);
     return b.bundle()
         .pipe(source('scripts.concat.js'))
         .pipe(rename('script.js'))
         .pipe(gulp.dest('./'))
+});
+
+gulp.task('scriptsmin', function(){
+    return browserify(MAIN_JS_FILE)
+        .bundle()
+        .pipe(source('scripts.concatmin.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(rename('script-min.js'))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('sass', function(){
@@ -20,7 +34,7 @@ gulp.task('sass', function(){
 })
 
 gulp.task('default', function() {
-    gulp.start(['scripts', 'sass']);
-    gulp.watch(['./js/**/*.js'], ['scripts']);
+    gulp.start(['scripts', 'scriptsmin', 'sass']);
+    gulp.watch(['./js/**/*.js'], ['scripts', 'scriptsmin']);
     gulp.watch(['./sass/**/*.scss', './sass/**/*.css'], ['sass']);
 });
--- script.js
+++ script.js
@@ -14,6 +14,7 @@ module.exports = {
 var example = require('./lib/example');
 var exampleUsingExport = require('./lib/exampleUsingExport');
 
+
 exampleUsingExport.test();
 },{"./lib/example":1,"./lib/exampleUsingExport":2}],4:[function(require,module,exports){
 /*eslint-disable no-unused-vars*/
